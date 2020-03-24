3d79bd0385ebca957a1f688994613df0ecb4ae35
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-03-24 20:15:36 +0900

    バックエンドのエラー対策待ち

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
-import Login from "./components/Login/Login";
+import Login from "./screens/Login/Login";
 import { Route, Switch } from "react-router-dom";
 import Title from "./components/Title/Title";
-import Signup from "./components/Signup/Signup";
+import Signup from "./screens/Signup/Signup";
 import Matches from "./components/Matches/Matches";
+import Profile from "./screens/Profile/Profile";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/Matches" component={Matches} />
+        <Route path="/profile" component={Profile} />
         <Route path="/" exact component={Title} />
       </Switch>
     </div>
--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-export default class Login extends Component {
+import { Button } from "react-bootstrap";
+import axios from "../../axios";
+export default class Profile extends Component {
+  state = {
+    images: null
+  };
+  fileSelectedHandler = event => {
+    this.setState({
+      images: event.target.files[0]
+    });
+  };
+  fileUploadHandler = () => {
+    const fd = new FormData();
+    fd.append("images", this.state.images, this.state.images.name);
+    axios.post("/me/upload_image", fd).then(res => {
+      console.log(res);
+    });
+  };
   render() {
     return (
       <div>
-        <input type="file" />
+        <input type="file" onChange={this.fileSelectedHandler} />
+        <Button onClick={this.fileUploadHandler} variant="danger">
+          登録
+        </Button>
       </div>
     );
   }
