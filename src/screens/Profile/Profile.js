ea7895b001fa9da1915ef61a694398374e562dc4
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-03-24 14:12:04 +0900

    ファイル保存

---
+++ src/screens/Profile/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <input type="file" />
+      </div>
+    );
+  }
+}
