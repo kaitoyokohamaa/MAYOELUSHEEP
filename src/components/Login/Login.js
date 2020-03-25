cc1f1da8746d16f2e4d3800279fa45dff8078b81
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-03-25 15:03:21 +0900

    ログイン機能を作りました

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,169 +1,123 @@
 import React, { Component } from "react";
-import "./Login.module.css";
-import Button from "../UI/Button/Button";
-import Input from "../UI/Input/Input";
-import classes from "./Login.module.css";
-import { Redirect, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import { Redirect } from "react-router-dom";
+import LOGO from "../../assets/img/logo.jpg";
+import TOP from "../../assets/img/top.jpg";
+import axios from "../../axios";
 class Login extends Component {
-  state = {
-    controls: {
-      email: {
-        elementType: "input",
-        elementConfig: {
-          type: "email",
-          placeholder: "Mail Address"
-        },
-        value: "",
-        validation: {
-          required: true,
-          isEmail: true
-        },
-        valid: false,
-        touched: false
-      },
-      password: {
-        elementType: "input",
-        elementConfig: {
-          type: "password",
-          placeholder: "Password"
-        },
-        value: "",
-        validation: {
-          required: true,
-          minLength: 6
-        },
-        valid: false,
-        touched: false
-      },
-      passwordre: {
-        elementType: "input",
-        elementConfig: {
-          type: "password",
-          placeholder: "Password"
-        },
-        value: "",
-        validation: {
-          required: true,
-          minLength: 6
-        },
-        valid: false,
-        touched: false
-      }
-    },
-    redirectToReferrer: false
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      logined: false
+    };
+    this.login = this.login.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
 
-  SignIn = event => {
-    const urlsign = "https://teachapi.herokuapp.com/sign_in";
+  login(event) {
     event.preventDefault();
-    alert("loh");
     let data = {
       sign_in_user_params: {
-        email: this.state.controls.email.value,
-        password: this.state.controls.password.value,
-        password_confirmation: this.state.controls.passwordre.value
+        email: this.state.email,
+        password: this.state.password,
+        password_confirmation: this.state.password
       }
     };
-    fetch(urlsign, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    })
-      .then(response => response.json())
+    axios
+      .post("/sign_in", data)
       .then(json => {
+        alert("loh");
         //ユーザ生成時に以下の情報をローカルストレージに入れる。
-        console.log(json);
-        window.localStorage.setItem("token", json.token);
-        window.localStorage.setItem("id", json.id);
-        window.localStorage.setItem("token", json.token);
-      })
-      .then(responseData => {
-        console.log(responseData);
+        console.log(json.data);
+        window.localStorage.setItem("token", json.data.token);
+        window.localStorage.setItem("id", json.data.id);
+        if (json.status === 200) {
+          this.setState({ logined: true });
+        }
       })
       .catch(err => {
         console.log(err, err.data);
       });
-    if (localStorage.token) {
-      this.setState({ redirectToReferrer: true });
-    }
-  };
-
-  checkValidity(value, rules) {
-    let isValid = true;
-    if (!rules) {
-      return true;
-    }
-    if (rules.required) {
-      isValid = value.trim() !== "" && isValid;
-    }
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-    return isValid;
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  onChange = (event, controlName) => {
-    const updatedControls = {
-      ...this.state.controls,
-      [controlName]: {
-        ...this.state.controls[controlName],
-        value: event.target.value,
-        valid: this.checkValidity(
-          event.target.value,
-          this.state.controls[controlName].validation
-        ),
-        touched: true
-      }
-    };
-    this.setState({ controls: updatedControls });
-  };
-
   render() {
-    if (this.state.redirectToReferrer) {
-      return <Redirect to={"/home/"} />;
-    }
-    const formElementsArray = [];
-    for (let key in this.state.controls) {
-      formElementsArray.push({
-        id: key,
-        config: this.state.controls[key]
-      });
-    }
-    const form = formElementsArray.map(formElement => (
-      <Input
-        key={formElement.id}
-        elementType={formElement.config.elementType}
-        elementConfig={formElement.config.elementConfig}
-        value={formElement.config.value}
-        invalid={!formElement.config.valid}
-        shouldValidate={formElement.config.validation}
-        touched={formElement.config.touched}
-        changed={event => this.onChange(event, formElement.id)}
-      />
-    ));
     return (
       <div>
-        <p>お帰りなさい！</p>
-        <div className={classes.Auth}>
-          <form>
-            {form}
-            <Button clicked={this.SignIn} btnType="Success">
-              {" "}
-              ログイン
-            </Button>
-          </form>
-          <div className={classes.NavigationItem}>
-            <Link to="/signin">新規登録</Link>
-          </div>
-        </div>
+        {this.state.logined ? (
+          <Redirect to={"/home"} />
+        ) : (
+          <Container style={{ textAlign: "left" }}>
+            <Row>
+              <Col lg={5}>
+                <Form style={{ marginTop: "30%", marginRight: "28%" }}>
+                  <img src={LOGO} alt="MyLogo" />
+                  <p
+                    style={{
+                      fontSize: "20px",
+                      fontFamily: "Arial",
+                      color: "gray",
+                      marginTop: "40px"
+                    }}
+                  >
+                    おかえりなさい！
+                  </p>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label
+                      style={{
+                        marginTop: "30px"
+                      }}
+                    >
+                      メールアドレス
+                    </Form.Label>
+                    <Form.Control
+                      name="email"
+                      type="email"
+                      placeholder="Enter email"
+                      onChange={this.onChange}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>パスワード</Form.Label>
+                    <Form.Control
+                      name="password"
+                      type="password"
+                      placeholder="Password"
+                      onChange={this.onChange}
+                    />
+                  </Form.Group>
+                  <Button
+                    variant="danger"
+                    type="submit"
+                    onClick={this.login}
+                    style={{
+                      marginTop: "20px"
+                    }}
+                  >
+                    ログイン
+                  </Button>
+                </Form>
+              </Col>
+              <Col lg={7}>
+                <img
+                  style={{ maxWidth: "800px", height: "100vh", margin: "0" }}
+                  src={TOP}
+                  alt="toppic"
+                />
+              </Col>
+            </Row>
+          </Container>
+        )}
       </div>
     );
   }
 }
+
 export default Login;
