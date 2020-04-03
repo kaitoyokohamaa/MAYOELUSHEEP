1cdcb73dd0b75432517724ecaab8c8cd41f42bf3
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-04-03 13:30:11 +0900

    profileのプログレスバー追加

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "../../axios";
 import LOGO from "../../assets/img/logo.jpg";
-import { Col, Row, Container, Button } from "react-bootstrap";
+import { Col, Row, Container, Button, ProgressBar } from "react-bootstrap";
+import "./Profile.css";
 export default class Profile extends Component {
   state = {
     images: null
@@ -21,64 +22,79 @@ export default class Profile extends Component {
   };
   render() {
     return (
-      <React.Fragment>
+      <div
+        style={{
+          textAlign: "left",
+          paddingLeft: "20px"
+        }}
+      >
         <div
           style={{
-            textAlign: "left",
-            paddingTop: "20px",
-            paddingLeft: "20px"
+            display: "flex"
           }}
         >
-          <img src={LOGO} alt="logo" />
-        </div>
-        <Container
-          style={{
-            textAlign: "center"
-          }}
-        >
-          <Row>
-            <Col>
-              <div>
-                <label
-                  htmlFor="file_upload"
-                  onChange={this.fileSelectedHandler}
-                  style={{
-                    width: "30%",
-                    backgroundColor: "#ececec",
-                    borderRadius: "9%",
-                    border: "1px solid",
-                    padding: "21% 10%",
-                    fontSize: "45px",
-                    color: "#fff",
-                    marginTop: "5%"
-                  }}
-                >
-                  +
-                </label>
-                <input
-                  style={{ padding: "25%", display: "none" }}
-                  id="file_upload"
-                  type="file"
-                  multiple
-                  onChange={this.fileSelectedHandler}
-                />
-              </div>
-
-              <Button
-                variant="danger"
+          <div className="progressbarstyle">
+            <div>
+              <img
+                src={LOGO}
+                alt="logo"
                 style={{
-                  width: "25%",
-                  margin: "20px auto"
+                  width: "250px",
+                  paddingTop: "20px"
                 }}
-                type="button"
-                onClick={this.fileUploadHandler}
-              >
-                登録
-              </Button>
-            </Col>
-          </Row>
-        </Container>
-      </React.Fragment>
+              />
+            </div>
+            <ProgressBar animated now={45} />
+          </div>
+          <Container
+            style={{
+              textAlign: "center"
+            }}
+          >
+            <Row>
+              <Col>
+                <div>
+                  <label
+                    htmlFor="file_upload"
+                    onChange={this.fileSelectedHandler}
+                    style={{
+                      width: "30%",
+                      backgroundColor: "#ececec",
+                      borderRadius: "9%",
+                      border: "1px solid",
+                      padding: "21% 10%",
+                      fontSize: "45px",
+                      color: "#fff",
+                      marginTop: "5%"
+                    }}
+                  >
+                    +
+                  </label>
+                  <input
+                    style={{ padding: "25%", display: "none" }}
+                    id="file_upload"
+                    type="file"
+                    multiple
+                    onChange={this.fileSelectedHandler}
+                  />
+                </div>
+
+                <Button
+                  variant="danger"
+                  style={{
+                    width: "25%",
+                    margin: "20px auto"
+                  }}
+                  type="button"
+                  onClick={this.fileUploadHandler}
+                >
+                  登録
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
     );
   }
 }
