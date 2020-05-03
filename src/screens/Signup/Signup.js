b9ba8b8c6716b218eecd002e6ba2d0c8f45ac159
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-05-03 18:00:56 +0900

    loading

--- src/screens/Signup/Signup.js
+++ src/screens/Signup/Signup.js
@@ -15,11 +15,12 @@ const Signup = () => {
   const [Gender, setGender] = useState("");
   const [Password, setPassword] = useState("");
   const [Password_confirm, setPassword_confirm] = useState("");
-
+  const [Loading, setLoading] = useState(true);
   const [Auth, setAuth] = useState(false);
 
   const signup = event => {
     event.preventDefault();
+    setLoading(false);
     let data = {
       user_params: {
         name: Name,
@@ -40,16 +41,20 @@ const Signup = () => {
         window.localStorage.setItem("id", json.data.id);
 
         if (formIsValid() && json.status === 200) {
+          setLoading(true);
           setAuth(true);
         } else {
-          alert("パスワードが間違ってます");
+          setLoading(true);
+          alert("ちゃんと入力してください");
         }
       })
       .then(responseData => {
         console.log(responseData);
       })
       .catch(err => {
+        setLoading(true);
         console.log(err, err.data);
+        alert("エラーです");
       });
     console.log("RENDRING INGREDIENTS", Name);
   };
@@ -63,117 +68,228 @@ const Signup = () => {
   console.log(Password_confirm);
   return (
     <div>
-      {Auth ? (
-        <Redirect to={"/profile"} />
+      {Loading ? (
+        <div>
+          <Container style={{ textAlign: "left" }}>
+            <Row>
+              <Col lg={5}>
+                <Form className={classes.form}>
+                  <img src={LOGO} alt="MyLogo" />
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>お名前</Form.Label>
+                    <Form.Control
+                      name="name"
+                      value={Name}
+                      type="name"
+                      placeholder="name"
+                      onChange={event => {
+                        setName(event.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>メールアドレス</Form.Label>
+                    <Form.Control
+                      name="email"
+                      value={Email}
+                      type="email"
+                      placeholder="Enter email"
+                      onChange={event => {
+                        setEmail(event.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="exampleForm.ControlSelect1">
+                    <Form.Label>年齢</Form.Label>
+                    <Form.Control
+                      as="select"
+                      name="old"
+                      value={Old}
+                      onChange={event => {
+                        setOld(event.target.value);
+                      }}
+                    >
+                      <option>選んでください！</option>
+                      <option>18</option>
+                      <option>19</option>
+                      <option>20</option>
+                      <option>21</option>
+                      <option>22</option>
+                      <option>23</option>
+                      <option>24</option>
+                      <option>25</option>
+                      <option>26</option>
+                      <option>27</option>
+                      <option>28</option>
+                      <option>29</option>
+                      <option>30</option>
+                    </Form.Control>
+                    <Form.Label>性別</Form.Label>
+                    <Form.Control
+                      as="select"
+                      name="gender"
+                      value={Gender}
+                      onChange={event => {
+                        setGender(event.target.value);
+                      }}
+                    >
+                      <option>選んでください！</option>
+                      <option>男</option>
+                      <option>女</option>
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>パスワード</Form.Label>
+                    <Form.Control
+                      name="password"
+                      type="password"
+                      value={Password}
+                      placeholder="Password"
+                      onChange={event => {
+                        setPassword(event.target.value);
+                      }}
+                    />
+                    <Form.Label>パスワード再確認</Form.Label>
+                    <Form.Control
+                      name="password_confirm"
+                      type="password"
+                      value={Password_confirm}
+                      placeholder="Password"
+                      onChange={event => {
+                        setPassword_confirm(event.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <div className={classes.submitbtn}>
+                    <Button
+                      className={classes.submitbtn_size}
+                      variant="danger"
+                      type="submit"
+                      onClick={signup}
+                    >
+                      始める
+                    </Button>
+                  </div>
+                </Form>
+              </Col>
+              <Col lg={7}>
+                <img className={classes.main_img} src={TOP} alt="toppic" />
+              </Col>
+            </Row>
+          </Container>
+        </div>
       ) : (
-        <Container style={{ textAlign: "left" }}>
-          <Row>
-            <Col lg={5}>
-              <Form className={classes.form}>
-                <img src={LOGO} alt="MyLogo" />
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>お名前</Form.Label>
-                  <Form.Control
-                    name="name"
-                    value={Name}
-                    type="name"
-                    placeholder="name"
-                    onChange={event => {
-                      setName(event.target.value);
-                    }}
-                  />
-                </Form.Group>
-                <Form.Group controlId="formBasicEmail">
-                  <Form.Label>メールアドレス</Form.Label>
-                  <Form.Control
-                    name="email"
-                    value={Email}
-                    type="email"
-                    placeholder="Enter email"
-                    onChange={event => {
-                      setEmail(event.target.value);
-                    }}
-                  />
-                </Form.Group>
-                <Form.Group controlId="exampleForm.ControlSelect1">
-                  <Form.Label>年齢</Form.Label>
-                  <Form.Control
-                    as="select"
-                    name="old"
-                    value={Old}
-                    onChange={event => {
-                      setOld(event.target.value);
-                    }}
-                  >
-                    <option>選んでください！</option>
-                    <option>18</option>
-                    <option>19</option>
-                    <option>20</option>
-                    <option>21</option>
-                    <option>22</option>
-                    <option>23</option>
-                    <option>24</option>
-                    <option>25</option>
-                    <option>26</option>
-                    <option>27</option>
-                    <option>28</option>
-                    <option>29</option>
-                    <option>30</option>
-                  </Form.Control>
-                  <Form.Label>性別</Form.Label>
-                  <Form.Control
-                    as="select"
-                    name="gender"
-                    value={Gender}
-                    onChange={event => {
-                      setGender(event.target.value);
-                    }}
-                  >
-                    <option>選んでください！</option>
-                    <option>男</option>
-                    <option>女</option>
-                  </Form.Control>
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                  <Form.Label>パスワード</Form.Label>
-                  <Form.Control
-                    name="password"
-                    type="password"
-                    value={Password}
-                    placeholder="Password"
-                    onChange={event => {
-                      setPassword(event.target.value);
-                    }}
-                  />
-                  <Form.Label>パスワード</Form.Label>
-                  <Form.Control
-                    name="password_confirm"
-                    type="password"
-                    value={Password_confirm}
-                    placeholder="Password"
-                    onChange={event => {
-                      setPassword_confirm(event.target.value);
-                    }}
-                  />
-                </Form.Group>
-                <div className={classes.submitbtn}>
-                  <Button
-                    className={classes.submitbtn_size}
-                    variant="danger"
-                    type="submit"
-                    onClick={signup}
-                  >
-                    始める
-                  </Button>
-                </div>
-              </Form>
-            </Col>
-            <Col lg={7}>
-              <img className={classes.main_img} src={TOP} alt="toppic" />
-            </Col>
-          </Row>
-        </Container>
+        <div>
+          <Container style={{ textAlign: "left" }}>
+            <Row>
+              <Col lg={5}>
+                <Form className={classes.form}>
+                  <img src={LOGO} alt="MyLogo" />
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>お名前</Form.Label>
+                    <Form.Control
+                      name="name"
+                      value={Name}
+                      type="name"
+                      placeholder="name"
+                      onChange={event => {
+                        setName(event.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>メールアドレス</Form.Label>
+                    <Form.Control
+                      name="email"
+                      value={Email}
+                      type="email"
+                      placeholder="Enter email"
+                      onChange={event => {
+                        setEmail(event.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group controlId="exampleForm.ControlSelect1">
+                    <Form.Label>年齢</Form.Label>
+                    <Form.Control
+                      as="select"
+                      name="old"
+                      value={Old}
+                      onChange={event => {
+                        setOld(event.target.value);
+                      }}
+                    >
+                      <option>選んでください！</option>
+                      <option>18</option>
+                      <option>19</option>
+                      <option>20</option>
+                      <option>21</option>
+                      <option>22</option>
+                      <option>23</option>
+                      <option>24</option>
+                      <option>25</option>
+                      <option>26</option>
+                      <option>27</option>
+                      <option>28</option>
+                      <option>29</option>
+                      <option>30</option>
+                    </Form.Control>
+                    <Form.Label>性別</Form.Label>
+                    <Form.Control
+                      as="select"
+                      name="gender"
+                      value={Gender}
+                      onChange={event => {
+                        setGender(event.target.value);
+                      }}
+                    >
+                      <option>選んでください！</option>
+                      <option>男</option>
+                      <option>女</option>
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group controlId="formBasicPassword">
+                    <Form.Label>パスワード</Form.Label>
+                    <Form.Control
+                      name="password"
+                      type="password"
+                      value={Password}
+                      placeholder="Password"
+                      onChange={event => {
+                        setPassword(event.target.value);
+                      }}
+                    />
+                    <Form.Label>パスワード再確認</Form.Label>
+                    <Form.Control
+                      name="password_confirm"
+                      type="password"
+                      value={Password_confirm}
+                      placeholder="Password"
+                      onChange={event => {
+                        setPassword_confirm(event.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <div className={classes.submitbtn}>
+                    <Button
+                      className={classes.submitbtn_size}
+                      variant="danger"
+                      type="submit"
+                      onClick={signup}
+                    >
+                      Loading
+                    </Button>
+                  </div>
+                </Form>
+              </Col>
+              <Col lg={7}>
+                <img className={classes.main_img} src={TOP} alt="toppic" />
+              </Col>
+            </Row>
+          </Container>
+        </div>
       )}
+      {Auth ? <Redirect to={"/profile"} /> : null}
     </div>
   );
 };
